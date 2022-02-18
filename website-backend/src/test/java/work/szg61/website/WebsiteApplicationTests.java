4ebfea055554bd8089da4cc5163a317a5907fa29
shenzhuge <shenzhuge@qq.com>
2022-02-18 19:29:27 +0800

    first commit

---
+++ website-backend/src/test/java/work/szg61/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package work.szg61.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
